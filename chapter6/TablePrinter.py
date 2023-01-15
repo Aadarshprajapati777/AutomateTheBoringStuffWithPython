64f9b4f8c7ec23ea899fa6661a62f2d577f42e3c
Aadarsh <aadarsh9811594152@gmail.com>
2023-01-15 20:12:18 +0530

    rjust need to be added

--- chapter6/TablePrinter.py
+++ chapter6/TablePrinter.py
@@ -1,11 +1,10 @@
-list=[]
-while True:
-    n=(input("enter a num (enter nothing to stop): "))
-    if(n==''):
-        break
-    list.append(n)
-print(list[-1])
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
 
 
-
-
+for i in range(len(tableData[1])):
+    for j in range(len(tableData)):
+        print(tableData[j][i],end='')
+    print()
+    
